29ffb4aaa33e030982b51a26041c4a1019b8cf9b
WilliamGathii <billgathii@gmail.com>
2026-07-27 11:03:25 +0300

    Add API router

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const healthRoutes = require('./health.routes');
+
 const router = express.Router();
+
+router.use('/health', healthRoutes);
 
 module.exports = router;
